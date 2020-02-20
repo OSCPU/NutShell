d532ee1da4aeea158a52775d1b025d5e8f170ba0
William Wang <zeweiwang@outlook.com>
2020-02-20 22:33:09 +0800

    add(ROB): add prf and redirect logic to ROB

--- src/main/scala/noop/RMT.scala
+++ src/main/scala/noop/RMT.scala
@@ -6,10 +6,10 @@ import chisel3.util.experimental.BoringUtils
 
 import utils._
 
-// RMT! RMT! RMT!
-
 trait HasRMTConst{
-  val rmtWidth = 2
+  val rmtReadWidth = 4
+  val rmtWriteWidth = 2
+  val rmtRetireWidth = 2
 }
 
 // Register File Map Table
@@ -18,24 +18,45 @@ trait HasRMTConst{
 class RMT extends NOOPModule with HasROBConst with HasRegFileParameter with HasRMTConst{
   val io = IO(new Bundle {
     // write
-    val wen = Input(Vec(rmtWidth, Bool()))
-    val warf = Input(UInt(5.W))
-    val wprf = Input(UInt(prfAddrWidth.W))
+    val wen = Input(Vec(rmtWriteWidth, Bool()))
+    val warf = Input(Vec(rmtWriteWidth, UInt(5.W)))
+    val wprf = Input(Vec(rmtWriteWidth, UInt(prfAddrWidth.W)))
     // read
-    val rarf = Input(Vec(rmtWidth, UInt(5.W)))
-    val rprf = Output(Vec(rmtWidth, UInt(prfAddrWidth.W)))
+    val rarf = Input(Vec(rmtReadWidth, UInt(5.W)))
+    val rprf = Output(Vec(rmtReadWidth, UInt(prfAddrWidth.W)))
+    val rvalid = Output(Vec(rmtReadWidth, Bool()))
+    val rcommited = Output(Vec(rmtReadWidth, Bool()))
+    // commit
+    val cen = Input(Vec(rmtRetireWidth, Bool()))
+    val cprf = Input(Vec(rmtRetireWidth, UInt(5.W)))
+    // retire
+    val rten = Input(Vec(rmtRetireWidth, Bool()))
+    val rtprf = Input(Vec(rmtRetireWidth, UInt(5.W)))
+    
     val flush = Input(Bool())
   })
 
   val map = Mem(NRReg, UInt(log2Up(robSize).W))
   val valid = Vec(NRReg, RegInit(false.B))
+  val commited = Vec(NRReg, RegInit(false.B))
+
+  List.tabulate(rmtRetireWidth)(i => when(io.rten(i)){valid(io.rtprf(i)) := false.B})
+  List.tabulate(rmtRetireWidth)(i => when(io.cen(i)){commited(io.cprf(i)) := true.B})
 
   when(io.flush){
     List.tabulate(NRReg)(i => valid(i) := false.B) // set valid to 0
   }.otherwise{
-    List.tabulate(robWidth)(i => when(io.wen(i)){map(io.warf(i)) := io.wprf(i)})
+    List.tabulate(rmtWriteWidth)(i => when(io.wen(i)){
+      map(io.warf(i)) := io.wprf(i)
+      valid(io.warf(i)) := true.B
+      commited(io.warf(i)) := false.B
+    })
   }
 
-  List.tabulate(robWidth)(i => io.rprf(i) := map(io.rarf(i)))
+  List.tabulate(rmtReadWidth)(i => {
+    io.rprf(i) := map(io.rarf(i))
+    io.rvalid(i) := valid(io.rarf(i))
+    io.rcommited(i) := commited(io.rarf(i))
+  })
 
 }
