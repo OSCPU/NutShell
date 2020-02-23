31dedee8a6a951645a5dcbf5c04dcc8189105bdf
William Wang <zeweiwang@outlook.com>
2020-02-24 00:47:46 +0800

    add(TOP): add out of order pipeline

--- src/main/scala/noop/NOOP.scala
+++ src/main/scala/noop/NOOP.scala
@@ -24,7 +24,7 @@ trait HasNOOPParameter {
   val DataBytes = DataBits / 8
   val EnableMultiIssue = true
   val EnableSuperScalarExec = true
-  val EnableOutOfOrderExec = false
+  val EnableOutOfOrderExec = true
 }
 
 trait HasNOOPConst {
@@ -57,67 +57,100 @@ class NOOP(implicit val p: NOOPConfig) extends NOOPModule {
     val frontend = Flipped(new SimpleBusUC)
   })
 
-  val ifu  = Module(new IFU)
-  val ibf = Module(new IBF)
-  val idu  = Module(new IDU)
-  val isu  = Module(new ISU)
-  val exu  = Module(new EXU)
-  val wbu  = Module(new WBU)
-
   def pipelineConnect2[T <: Data](left: DecoupledIO[T], right: DecoupledIO[T],
     isFlush: Bool, entries: Int = 4, pipe: Boolean = false) = {
     right <> FlushableQueue(left, isFlush,  entries = entries, pipe = pipe)
   }
 
+  val ifu  = Module(new IFU)
+  val ibf = Module(new IBF)
+  val idu  = Module(new IDU)
+
   pipelineConnect2(ifu.io.out, ibf.io.in, ifu.io.flushVec(0))
   PipelineVector2Connect(new CtrlFlowIO, ibf.io.out(0), ibf.io.out(1), idu.io.in(0), idu.io.in(1), ifu.io.flushVec(1), 8)
-  PipelineVector2Connect(new DecodeIO, idu.io.out(0), idu.io.out(1), isu.io.in(0), isu.io.in(1), ifu.io.flushVec(1), 16)
-  PipelineConnect(isu.io.out, exu.io.in, exu.io.out.fire(), ifu.io.flushVec(2))
-  PipelineConnect(exu.io.out, wbu.io.in, true.B, ifu.io.flushVec(3))
   ibf.io.flush := ifu.io.flushVec(1)
   idu.io.flush := ifu.io.flushVec(1)
-  isu.io.flush := ifu.io.flushVec(2)
-  exu.io.flush := ifu.io.flushVec(3)
-
+  
   Debug() {
-    printf("------------------------ TIMER: %d ------------------------\n", GTimer())
-    printf("flush = %b, ifu:(%d,%d), ibf:(%d,%d), idu:(%d,%d), isu:(%d,%d), exu:(%d,%d), wbu: (%d,%d)\n",
+    printf("------------------------ FRONTEND: %d ------------------------\n", GTimer())
+    printf("flush = %b, ifu:(%d,%d), ibf:(%d,%d), idu:(%d,%d)\n",
       ifu.io.flushVec.asUInt, ifu.io.out.valid, ifu.io.out.ready,
-      ibf.io.in.valid, ibf.io.in.ready, idu.io.in(0).valid, idu.io.in(0).ready, isu.io.in(0).valid, isu.io.in(0).ready,
-      exu.io.in.valid, exu.io.in.ready, wbu.io.in.valid, wbu.io.in.ready)
+      ibf.io.in.valid, ibf.io.in.ready, idu.io.in(0).valid, idu.io.in(0).ready)
     when (ifu.io.out.valid) { printf("IFU: pc = 0x%x, instr = 0x%x\n", ifu.io.out.bits.pc, ifu.io.out.bits.instr)} ; 
     when (ibf.io.in.valid) { printf("IBF: pc = 0x%x, instr = 0x%x\n", ibf.io.in.bits.pc, ibf.io.in.bits.instr)}
     when (idu.io.in(0).valid) { printf("IDU1: pc = 0x%x, instr = 0x%x, pnpc = 0x%x\n", idu.io.in(0).bits.pc, idu.io.in(0).bits.instr, idu.io.in(0).bits.pnpc) }
     when (idu.io.in(1).valid) { printf("IDU2: pc = 0x%x, instr = 0x%x, pnpc = 0x%x\n", idu.io.in(1).bits.pc, idu.io.in(1).bits.instr, idu.io.in(1).bits.pnpc) }
-    when (isu.io.in(0).valid)  { printf("ISU: pc = 0x%x, pnpc = 0x%x\n", isu.io.in(0).bits.cf.pc, isu.io.in(0).bits.cf.pnpc)} ;
-    when (exu.io.in.valid)  { printf("EXU: pc = 0x%x, pnpc = 0x%x\n", exu.io.in.bits(0).cf.pc, exu.io.in.bits(0).cf.pnpc)} ;
-    when (wbu.io.in.valid)  { printf("WBU: pc = 0x%x rfWen:%d rfDest:%d rfData:%x Futype:%x\n", wbu.io.in.bits(0).decode.cf.pc, wbu.io.in.bits(0).decode.ctrl.rfWen, wbu.io.in.bits(0).decode.ctrl.rfDest, wbu.io.wb(0).rfData, wbu.io.in.bits(0).decode.ctrl.fuType )}
-    // when (io.in.valid) { printf("TIMER: %d WBU: pc = 0x%x wen %x wdata %x mmio %x intrNO %x\n", GTimer(), io.in.bits.decode.cf.pc, io.wb.rfWen, io.wb.rfData, io.in.bits.isMMIO, io.in.bits.intrNO) }
-    
-    // printf(p"IFUO: redirectIO:${ifu.io.out.bits.redirect}\n") ; printf("IFUO: exceptionVec: %x\n", ifu.io.out.bits.exceptionVec.asUInt)} 
-    // printf(p"IDUO: redirectIO:${idu.io.out.bits.cf.redirect} redirectIOC:${idu.io.redirect}\n") ; printf("IDUO: exceptionVec:%x\n", idu.io.out.bits.cf.exceptionVec.asUInt)}
-    // printf(p"ISUO: ${isu.io.out.bits.cf.redirect}\n") ; printf("ISUO: exceptionVec:%x\n", isu.io.out.bits.cf.exceptionVec.asUInt)}
-    // when (exu.io.out.bits.decode.cf.redirect.valid) { printf("EXUO: redirect valid:%d target:%x\n", exu.io.out.bits.decode.cf.redirect.valid, exu.io.out.bits.decode.cf.redirect.target) }
-    // when (wbu.io.in.valid) { printf("WBU: pc = 0x%x rfWen:%d rfDest:%d rfData:%x Futype:%x commits(0):%x commits(1):%x commits(3):%x\n", wbu.io.in.bits.decode.cf.pc, wbu.io.in.bits.decode.ctrl.rfWen, wbu.io.in.bits.decode.ctrl.rfDest, wbu.io.wb.rfData, wbu.io.in.bits.decode.ctrl.fuType, wbu.io.in.bits.commits(0), wbu.io.in.bits.commits(1), wbu.io.in.bits.commits(3)) }
-    
   }
-
-  isu.io.wb <> wbu.io.wb
-  ifu.io.redirect <> wbu.io.redirect
-  // forward
-  isu.io.forward <> exu.io.forward
 
   val mmioXbar = Module(new SimpleBusCrossbarNto1(if (HasDcache) 2 else 3))
   val dmemXbar = Module(new SimpleBusCrossbarNto1(4))
 
-  val itlb = TLB(in = ifu.io.imem, mem = dmemXbar.io.in(1), flush = ifu.io.flushVec(0) | ifu.io.bpFlush, csrMMU = exu.io.memMMU.imem)(TLBConfig(name = "itlb", userBits = ICacheUserBundleWidth, totalEntry = 4))
-  ifu.io.ipf := itlb.io.ipf
-  io.imem <> Cache(in = itlb.io.out, mmio = mmioXbar.io.in.take(1), flush = Fill(2, ifu.io.flushVec(0) | ifu.io.bpFlush), empty = itlb.io.cacheEmpty)(
-    CacheConfig(ro = true, name = "icache", userBits = ICacheUserBundleWidth))
-  
-  val dtlb = TLB(in = exu.io.dmem, mem = dmemXbar.io.in(2), flush = false.B, csrMMU = exu.io.memMMU.dmem)(TLBConfig(name = "dtlb", totalEntry = 64))
-  dmemXbar.io.in(0) <> dtlb.io.out
-  io.dmem <> Cache(in = dmemXbar.io.out, mmio = mmioXbar.io.in.drop(1), flush = "b00".U, empty = dtlb.io.cacheEmpty, enable = HasDcache)(CacheConfig(ro = false, name = "dcache"))
+  if(EnableOutOfOrderExec){
+    val backend = Module(new Backend)
+    // backend.io := DontCare // TODO: Delete it
+    PipelineVector2Connect(new DecodeIO, idu.io.out(0), idu.io.out(1), backend.io.in(0), backend.io.in(1), ifu.io.flushVec(1), 16)
+    backend.io.flush := ifu.io.flushVec(2)
+
+    val itlb = TLB(in = ifu.io.imem, mem = dmemXbar.io.in(1), flush = ifu.io.flushVec(0) | ifu.io.bpFlush, csrMMU = backend.io.memMMU.imem)(TLBConfig(name = "itlb", userBits = ICacheUserBundleWidth, totalEntry = 4))
+    ifu.io.ipf := itlb.io.ipf
+    io.imem <> Cache(in = itlb.io.out, mmio = mmioXbar.io.in.take(1), flush = Fill(2, ifu.io.flushVec(0) | ifu.io.bpFlush), empty = itlb.io.cacheEmpty)(
+      CacheConfig(ro = true, name = "icache", userBits = ICacheUserBundleWidth))
+    
+    val dtlb = TLB(in = backend.io.dmem, mem = dmemXbar.io.in(2), flush = false.B, csrMMU = backend.io.memMMU.dmem)(TLBConfig(name = "dtlb", totalEntry = 64))
+    dmemXbar.io.in(0) <> dtlb.io.out
+    io.dmem <> Cache(in = dmemXbar.io.out, mmio = mmioXbar.io.in.drop(1), flush = "b00".U, empty = dtlb.io.cacheEmpty, enable = HasDcache)(CacheConfig(ro = false, name = "dcache"))
+
+    ifu.io.redirect <> backend.io.redirect
+
+    Debug(){
+      printf("------------------------ BACKEND : %d ------------------------\n", GTimer())
+      //TODO
+    }
+  }else{
+    val isu  = Module(new ISU)
+    val exu  = Module(new EXU)
+    val wbu  = Module(new WBU)
+    PipelineVector2Connect(new DecodeIO, idu.io.out(0), idu.io.out(1), isu.io.in(0), isu.io.in(1), ifu.io.flushVec(1), 16)
+    PipelineConnect(isu.io.out, exu.io.in, exu.io.out.fire(), ifu.io.flushVec(2))
+    PipelineConnect(exu.io.out, wbu.io.in, true.B, ifu.io.flushVec(3))
+    isu.io.flush := ifu.io.flushVec(2)
+    exu.io.flush := ifu.io.flushVec(3)
+
+    Debug() {
+      printf("------------------------ TIMER: %d ------------------------\n", GTimer())
+      printf("flush = %b, ifu:(%d,%d), ibf:(%d,%d), idu:(%d,%d), isu:(%d,%d), exu:(%d,%d), wbu: (%d,%d)\n",
+        ifu.io.flushVec.asUInt, ifu.io.out.valid, ifu.io.out.ready,
+        ibf.io.in.valid, ibf.io.in.ready, idu.io.in(0).valid, idu.io.in(0).ready, isu.io.in(0).valid, isu.io.in(0).ready,
+        exu.io.in.valid, exu.io.in.ready, wbu.io.in.valid, wbu.io.in.ready)
+      when (ifu.io.out.valid) { printf("IFU: pc = 0x%x, instr = 0x%x\n", ifu.io.out.bits.pc, ifu.io.out.bits.instr)} ; 
+      when (ibf.io.in.valid) { printf("IBF: pc = 0x%x, instr = 0x%x\n", ibf.io.in.bits.pc, ibf.io.in.bits.instr)}
+      when (idu.io.in(0).valid) { printf("IDU1: pc = 0x%x, instr = 0x%x, pnpc = 0x%x\n", idu.io.in(0).bits.pc, idu.io.in(0).bits.instr, idu.io.in(0).bits.pnpc) }
+      when (idu.io.in(1).valid) { printf("IDU2: pc = 0x%x, instr = 0x%x, pnpc = 0x%x\n", idu.io.in(1).bits.pc, idu.io.in(1).bits.instr, idu.io.in(1).bits.pnpc) }
+      when (isu.io.in(0).valid)  { printf("ISU: pc = 0x%x, pnpc = 0x%x\n", isu.io.in(0).bits.cf.pc, isu.io.in(0).bits.cf.pnpc)} ;
+      when (exu.io.in.valid)  { printf("EXU: pc = 0x%x, pnpc = 0x%x\n", exu.io.in.bits(0).cf.pc, exu.io.in.bits(0).cf.pnpc)} ;
+      when (wbu.io.in.valid)  { printf("WBU: pc = 0x%x rfWen:%d rfDest:%d rfData:%x Futype:%x\n", wbu.io.in.bits(0).decode.cf.pc, wbu.io.in.bits(0).decode.ctrl.rfWen, wbu.io.in.bits(0).decode.ctrl.rfDest, wbu.io.wb(0).rfData, wbu.io.in.bits(0).decode.ctrl.fuType )}
+      // when (io.in.valid) { printf("TIMER: %d WBU: pc = 0x%x wen %x wdata %x mmio %x intrNO %x\n", GTimer(), io.in.bits.decode.cf.pc, io.wb.rfWen, io.wb.rfData, io.in.bits.isMMIO, io.in.bits.intrNO) }
+      // printf(p"IFUO: redirectIO:${ifu.io.out.bits.redirect}\n") ; printf("IFUO: exceptionVec: %x\n", ifu.io.out.bits.exceptionVec.asUInt)} 
+      // printf(p"IDUO: redirectIO:${idu.io.out.bits.cf.redirect} redirectIOC:${idu.io.redirect}\n") ; printf("IDUO: exceptionVec:%x\n", idu.io.out.bits.cf.exceptionVec.asUInt)}
+      // printf(p"ISUO: ${isu.io.out.bits.cf.redirect}\n") ; printf("ISUO: exceptionVec:%x\n", isu.io.out.bits.cf.exceptionVec.asUInt)}
+      // when (exu.io.out.bits.decode.cf.redirect.valid) { printf("EXUO: redirect valid:%d target:%x\n", exu.io.out.bits.decode.cf.redirect.valid, exu.io.out.bits.decode.cf.redirect.target) }
+      // when (wbu.io.in.valid) { printf("WBU: pc = 0x%x rfWen:%d rfDest:%d rfData:%x Futype:%x commits(0):%x commits(1):%x commits(3):%x\n", wbu.io.in.bits.decode.cf.pc, wbu.io.in.bits.decode.ctrl.rfWen, wbu.io.in.bits.decode.ctrl.rfDest, wbu.io.wb.rfData, wbu.io.in.bits.decode.ctrl.fuType, wbu.io.in.bits.commits(0), wbu.io.in.bits.commits(1), wbu.io.in.bits.commits(3)) }
+    }
+
+    isu.io.wb <> wbu.io.wb
+    ifu.io.redirect <> wbu.io.redirect
+    // forward
+    isu.io.forward <> exu.io.forward
+
+    val itlb = TLB(in = ifu.io.imem, mem = dmemXbar.io.in(1), flush = ifu.io.flushVec(0) | ifu.io.bpFlush, csrMMU = exu.io.memMMU.imem)(TLBConfig(name = "itlb", userBits = ICacheUserBundleWidth, totalEntry = 4))
+    ifu.io.ipf := itlb.io.ipf
+    io.imem <> Cache(in = itlb.io.out, mmio = mmioXbar.io.in.take(1), flush = Fill(2, ifu.io.flushVec(0) | ifu.io.bpFlush), empty = itlb.io.cacheEmpty)(
+      CacheConfig(ro = true, name = "icache", userBits = ICacheUserBundleWidth))
+    
+    val dtlb = TLB(in = exu.io.dmem, mem = dmemXbar.io.in(2), flush = false.B, csrMMU = exu.io.memMMU.dmem)(TLBConfig(name = "dtlb", totalEntry = 64))
+    dmemXbar.io.in(0) <> dtlb.io.out
+    io.dmem <> Cache(in = dmemXbar.io.out, mmio = mmioXbar.io.in.drop(1), flush = "b00".U, empty = dtlb.io.cacheEmpty, enable = HasDcache)(CacheConfig(ro = false, name = "dcache"))
+  }
 
   // Make DMA access through L1 DCache to keep coherence
   dmemXbar.io.in(3) <> io.frontend
