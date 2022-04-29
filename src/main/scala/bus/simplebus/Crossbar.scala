1b3b11828b85bdca4167d6221f6041e129563fe7
Donlon <idonlon@hotmail.com>
2022-04-29 22:26:23 +0800

    simplebus/crossbar: clear inflightSrc register during reset

--- src/main/scala/bus/simplebus/Crossbar.scala
+++ src/main/scala/bus/simplebus/Crossbar.scala
@@ -102,7 +102,7 @@ class SimpleBusCrossbarNto1(n: Int, userBits:Int = 0) extends Module {
   (inputArb.io.in zip io.in.map(_.req)).map{ case (arb, in) => arb <> in }
   val thisReq = inputArb.io.out
   assert(!(thisReq.valid && !thisReq.bits.isRead() && !thisReq.bits.isWrite()))
-  val inflightSrc = Reg(UInt(log2Up(n).W))
+  val inflightSrc = RegInit(0.U(log2Up(n).W))
 
   io.out.req.bits := thisReq.bits
   // bind correct valid and ready signals
