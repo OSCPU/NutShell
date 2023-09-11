425fe53d8670cea58f5d64254cc345d8f6bd62d3
Yinan Xu <xuyinan@ict.ac.cn>
2023-09-11 13:37:53 +0800

    Bump difftest with standard Mem interfaces

--- src/main/scala/device/AXI4RAM.scala
+++ src/main/scala/device/AXI4RAM.scala
@@ -23,19 +23,7 @@ import chisel3.util.experimental.loadMemoryFromFile
 import nutcore.HasNutCoreParameter
 import bus.axi4._
 import utils._
-
-class RAMHelper(memByte: Int) extends BlackBox with HasNutCoreParameter {
-  val io = IO(new Bundle {
-    val clk = Input(Clock())
-    val rIdx = Input(UInt(DataBits.W))
-    val rdata = Output(UInt(DataBits.W))
-    val wIdx = Input(UInt(DataBits.W))
-    val wdata = Input(UInt(DataBits.W))
-    val wmask = Input(UInt(DataBits.W))
-    val wen = Input(Bool())
-    val en = Input(Bool())
-  }).suggestName("io")
-}
+import difftest.common.DifftestMem
 
 class AXI4RAM[T <: AXI4Lite](_type: T = new AXI4, memByte: Int,
   useBlackBox: Boolean = false) extends AXI4SlaveModule(_type) with HasNutCoreParameter {
@@ -50,23 +38,24 @@ class AXI4RAM[T <: AXI4Lite](_type: T = new AXI4, memByte: Int,
   val wen = in.w.fire() && inRange(wIdx)
 
   val rdata = if (useBlackBox) {
-    val mem = Module(new RAMHelper(memByte))
-    mem.io.clk := clock
-    mem.io.rIdx := rIdx
-    mem.io.wIdx := wIdx
-    mem.io.wdata := in.w.bits.data
-    mem.io.wmask := fullMask
-    mem.io.wen := wen
-    mem.io.en := true.B
-    mem.io.rdata
+    val mem = DifftestMem(memByte, 8)
+    when (wen) {
+      mem.write(
+        addr = wIdx,
+        data = in.w.bits.data.asTypeOf(Vec(DataBytes, UInt(8.W))),
+        mask = in.w.bits.strb.asBools
+      )
+    }
+    mem.readAndHold(rIdx, ren).asUInt
   } else {
     val mem = Mem(memByte / DataBytes, Vec(DataBytes, UInt(8.W)))
 
     val wdata = VecInit.tabulate(DataBytes) { i => in.w.bits.data(8*(i+1)-1, 8*i) }
     when (wen) { mem.write(wIdx, wdata, in.w.bits.strb.asBools) }
 
-    Cat(mem.read(rIdx).reverse)
+    RegEnable(Cat(mem.read(rIdx).reverse), ren)
   }
 
-  in.r.bits.data := RegEnable(rdata, ren)
+  in.r.bits.data := rdata
 }
+
